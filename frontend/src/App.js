d0a32cb6744ef1c71cfc50c087d1cc46b343fdae
Shubham020303 <72245331+Shubham020303@users.noreply.github.com>
2023-03-27 06:41:48 +0000

    init commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
+// import React from 'react';
+// import VideoPlayer from './VideoPlayer/VideoPlayer';
+// import VideoPlayer2 from './VideoPlayer/VideoPlayer2';
+// import HomePage from './VideoPlayer/HomePage';
+
+// function App() {
+//   return (
+//     <>
+//       <HomePage />
+//     </>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VideoPlayer from './VideoPlayer/VideoPlayer';
 import VideoPlayer2 from './VideoPlayer/VideoPlayer2';
 import HomePage from './VideoPlayer/HomePage';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
-  return (
-    <>
-      {/* <VideoPlayer /> */}
-      {/* <VideoPlayer2 /> */}
-      <HomePage />
-    </>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route index path="/" element={<HomePage />} />
+				<Route path="/video1" component={VideoPlayer} />
+				<Route path="/video2" component={VideoPlayer2} />
+			</Routes>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,55 @@
 /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/*/.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Inter', 'Helvetica', 'sans-serif'],
+      body: ['Inter', 'Helvetica', 'sans-serif'],
+    },
+    extend: {
+      fontSize: {
+        14: '14px',
+      },
+      backgroundColor: {
+        'primary': '#1C2939',
+        'secondary': '#FF5D00',
+        'half-transparent': 'rgba(0, 0, 0, 0.5)',
+      },
+      colors: {
+        'primary': '#1C2939',
+        'secondary': '#FF5D00',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      borderColor: {
+        color: 'rgba(0, 0, 0, 0.1)',
+      },
+      width: {
+        400: '400px',
+        760: '760px',
+        780: '780px',
+        800: '800px',
+        1000: '1000px',
+        1200: '1200px',
+        1400: '1400px',
+      },
+      height: {
+        80: '80px',
+      },
+      minHeight: {
+        590: '590px',
+      },
+    },
   },
   plugins: [],
-}
+};
